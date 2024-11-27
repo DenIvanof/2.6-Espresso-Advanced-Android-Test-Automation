fe2cbc1d96b025665571a8350eef929e3b441448
Ivanich <deniska090798@gmail.com>
2024-11-27 21:27:24 +0400

    Intent

--- app/src/androidTest/java/ru/kkuzmichev/simpleappforespresso/intentTest.java
+++ app/src/androidTest/java/ru/kkuzmichev/simpleappforespresso/intentTest.java
@@ -1,47 +1,39 @@
 package ru.kkuzmichev.simpleappforespresso;
 
 
-import androidx.test.espresso.DataInteraction;
-import androidx.test.espresso.ViewInteraction;
-import androidx.test.filters.LargeTest;
-import androidx.test.ext.junit.rules.ActivityScenarioRule;
-import androidx.test.ext.junit.runners.AndroidJUnit4;
-
-import android.view.View;
-import android.view.ViewGroup;
-import android.view.ViewParent;
-
-import static androidx.test.InstrumentationRegistry.getInstrumentation;
-import static androidx.test.espresso.Espresso.onData;
 import static androidx.test.espresso.Espresso.onView;
-import static androidx.test.espresso.Espresso.pressBack;
-import static androidx.test.espresso.Espresso.openActionBarOverflowOrOptionsMenu;
-import static androidx.test.espresso.action.ViewActions.*;
-import static androidx.test.espresso.assertion.ViewAssertions.*;
-import static androidx.test.espresso.matcher.ViewMatchers.*;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.intent.Intents.intended;
+import static androidx.test.espresso.intent.matcher.IntentMatchers.hasAction;
+import static androidx.test.espresso.intent.matcher.IntentMatchers.hasData;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withContentDescription;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
 
-import io.qameta.allure.android.runners.AllureAndroidJUnit4;
-import ru.kkuzmichev.simpleappforespresso.R;
+import android.content.Intent;
 
-import org.hamcrest.Description;
-import org.hamcrest.Matcher;
-import org.hamcrest.TypeSafeMatcher;
-import org.hamcrest.core.IsInstanceOf;
+import androidx.test.espresso.ViewInteraction;
+import androidx.test.espresso.intent.Intents;
+import androidx.test.espresso.intent.rule.IntentsTestRule;
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.filters.LargeTest;
+
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import static org.hamcrest.Matchers.allOf;
-import static org.hamcrest.Matchers.anything;
-import static org.hamcrest.Matchers.is;
+import io.qameta.allure.android.runners.AllureAndroidJUnit4;
 
 @LargeTest
 @RunWith(AllureAndroidJUnit4.class)
-public class intentTest {
+public class IntentTest {
+    private static final String url = "https://google.com";
 
     @Rule
-    public ActivityScenarioRule<MainActivity> mActivityScenarioRule =
-            new ActivityScenarioRule<>(MainActivity.class);
+    public IntentsTestRule<MainActivity> mActivityScenarioRule =
+            new IntentsTestRule<>(MainActivity.class);
+
 
     @Test
     public void intentTest() {
@@ -53,5 +45,7 @@ public class intentTest {
         ViewInteraction materialTextView = onView(withText("Settings"));
         materialTextView.check(matches(isDisplayed()));
         materialTextView.perform(click());
+        intended(hasData(url));
+        intended(hasAction(Intent.ACTION_VIEW));
     }
 }
